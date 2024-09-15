68301133affb6a243df21eb585d4812a246f1f2e
Griffin Dixon <gtd0604@icloud.com>
2024-09-15 12:02:32 -0400

    Initialize inventory with product objects

---
+++ inventory_management.js
@@ -0,0 +1,32 @@
+// Creating an Array of Product Objects
+const inventory = [
+    {
+        name: "Water Bottle",
+        price: 1.49,
+        quantity: 500,
+        lowStockLevel: 100
+    },
+    {
+        name: "Laptop",
+        price: 299.99,
+        quantity: 50,
+        lowStockLevel: 5
+    },
+    {
+        name: "Gum",
+        price: 2.99,
+        quantity: 350,
+        lowStockLevel: 50
+    },
+    {
+        name: "Beer",
+        price: 21.99,
+        quantity: 75,
+        lowStockLevel: 15
+    },
+    {
+        name: "Apples",
+        price: 5.49,
+        quantity: 200,
+        lowStockLevel: 50
+    }];
